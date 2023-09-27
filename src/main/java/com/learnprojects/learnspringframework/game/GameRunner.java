586adf227003e04066daccdc50610bc8444f2656
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-27 12:47:37 +0200

    Created GameRunner and MarioGame class

---
+++ src/main/java/com/learnprojects/learnspringframework/game/GameRunner.java
@@ -0,0 +1,15 @@
+package com.learnprojects.learnspringframework.game;
+
+public class GameRunner {
+    private MarioGame game;
+    public GameRunner(MarioGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        game.up();
+        game.down();
+        game.right();
+        game.left();
+    }
+}
---
+++ src/main/java/com/learnprojects/learnspringframework/game/MarioGame.java
@@ -0,0 +1,19 @@
+package com.learnprojects.learnspringframework.game;
+
+public class MarioGame {
+    public void up() {
+        System.out.println("jump");
+    }
+
+    public void down() {
+        System.out.println("down into a hole");
+    }
+
+    public void right() {
+        System.out.println("accelerate");
+    }
+
+    public void left() {
+        System.out.println("stop");
+    }
+}
