348a33a759eebd2d09d13ce61f2f45a263b90aa9
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-27 14:09:28 +0200

    Added component annotation now is managed by spring context and implements interface

--- src/main/java/com/learnprojects/learnspringframework/game/MarioGame.java
+++ src/main/java/com/learnprojects/learnspringframework/game/MarioGame.java
@@ -1,6 +1,9 @@
 package com.learnprojects.learnspringframework.game;
 
-public class MarioGame {
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarioGame implements GamingConsole {
     public void up() {
         System.out.println("jump");
     }
@@ -9,11 +12,11 @@ public class MarioGame {
         System.out.println("down into a hole");
     }
 
-    public void right() {
-        System.out.println("accelerate");
-    }
-
     public void left() {
         System.out.println("stop");
+    }
+
+    public void right() {
+        System.out.println("accelerate");
     }
 }
