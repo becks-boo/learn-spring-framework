b2944c6722b7d3f0db2c7edb66d4f55dc98ada50
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-27 15:56:04 +0200

    Using and testing the web controller

--- src/main/java/com/learnprojects/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/learnprojects/learnspringframework/LearnSpringFrameworkApplication.java
@@ -1,6 +1,7 @@
 package com.learnprojects.learnspringframework;
 
-import com.learnprojects.learnspringframework.game.*;
+import com.learnprojects.learnspringframework.enterprise.example.web.MyWebController;
+import com.learnprojects.learnspringframework.game.GameRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,12 +11,11 @@ public class LearnSpringFrameworkApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 
-//		MarioGame game = new MarioGame();
-//		SuperContraGame game = new SuperContraGame();
-//		GamingConsole game = new MarioGame();
-//		GameRunner runner = new GameRunner(game);
 		GameRunner runner = context.getBean(GameRunner.class);
 		runner.run();
+
+		MyWebController controller = context.getBean(MyWebController.class);
+		System.out.println(controller.returnValueFromBusinessService());
 	}
 
 }
